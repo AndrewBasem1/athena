7394d887bb3eb8364806b4e74bd0b3aacdf4b848
andrew basem <andrew.basem@gmail.com>
2024-01-19 00:12:38 +0200

    adding db_engine.py

---
+++ db_engine.py
@@ -0,0 +1,14 @@
+from sqlmodel import create_engine
+from os import environ
+from dotenv import load_dotenv
+
+load_dotenv()
+DATABASE_HOST = environ["DATABASE_HOST"]
+DATABASE_PORT = environ["DATABASE_PORT"]
+DATABASE_USERNAME = environ["DATABASE_USERNAME"]
+DATABASE_PASSWORD = environ["DATABASE_PASSWORD"]
+DATABASE_SCHEMA = environ["DATABASE_SCHEMA"]
+
+database_connection_url = f"mysql+pymysql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_SCHEMA}"
+
+db_engine = create_engine(database_connection_url)
